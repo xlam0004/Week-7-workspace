47de24dc04a607fe00a38a28d7b3762578ff7e60
xlam0004 <113871296+xlam0004@users.noreply.github.com>
2022-09-25 00:21:36 +0800

    Update vega_lite_vis.js
    change

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,4 +1,4 @@
-var vg_1 = "https://github.com/xlam0004/Week-7-workspace/blob/main/daily_cost_bar_chart.vg.json";
+var vg_1 = "daily_cost_bar_chart.vg.json";
 
 // display at the bar_chart division, JSON file name
 vegaEmbed("#bar_chart", vg_1).then(function(result){}).catch(console.error);
