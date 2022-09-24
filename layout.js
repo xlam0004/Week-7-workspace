c9e95f41478dbef45e850a6fc0779cefd51b5284
xlam0004 <113871296+xlam0004@users.noreply.github.com>
2022-09-24 23:56:20 +0800

    Add files via upload
    week 7 testing

---
+++ layout.js
@@ -0,0 +1,4 @@
+var vg_1 = "layout.vg.json";
+
+// display at the bar_chart division, JSON file name
+vegaEmbed("#bar_chart", vg_1).then(function(result){}).catch(console.error);
---
+++ vega_lite_vis.js
@@ -0,0 +1,4 @@
+var vg_1 = "daily_cost_bar_chart.vg.json";
+
+// display at the bar_chart division, JSON file name
+vegaEmbed("#bar_chart", vg_1).then(function(result){}).catch(console.error);
